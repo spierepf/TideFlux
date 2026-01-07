08aa6fa9fcf62c5bf839bc4786fe666e9c89ecd1
Peter-Frank Spierenburg <spierepf@hotmail.com>
2026-01-06 22:04:22 -0400

    Added osc

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 
 #include "wifi.hpp"
+#include "osc.hpp"
 
 #ifndef SERIAL_SPEED
 #define SERIAL_SPEED 9600
 #endif
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(SERIAL_SPEED);
 
   wifi::setup();
+  osc::setup();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   wifi::loop();
+  osc::loop();
 }
